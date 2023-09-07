d01150ef19f9617b1eb81de6e69a9b4ebcbce418
Aadhi46 <144215399+Aadhi46@users.noreply.github.com>
2023-09-07 12:54:33 +0530

    Add files via upload

---
+++ calmain.py
@@ -0,0 +1,16 @@
+import cal1
+n=cal1.add(18,6)
+print("sum:",n)
+
+
+import cal1
+n=cal1.sub(18,6)
+print("difference:",n)
+
+import cal1
+n=cal1.pro(18,6)
+print("product:",n)
+
+import cal1
+n=cal1.div(18,6)
+print("Quotient:",n)
